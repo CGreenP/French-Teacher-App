0c44e5b47ecfa7ea9f21f04e59d5db0a348e84a4
Chayan Pal <chayan110201@gmail.com>
2023-10-04 17:07:14 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/frenchteacherapp/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.frenchteacherapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button blackBtn, greenBtn, purpleBtn, redBtn, yellowBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        blackBtn = findViewById(R.id.blackBtn);
+        greenBtn = findViewById(R.id.greenBtn);
+        purpleBtn = findViewById(R.id.purpleBtn);
+        redBtn = findViewById(R.id.redBtn);
+        yellowBtn = findViewById(R.id.yellowBtn);
+
+        blackBtn.setOnClickListener(this);
+        greenBtn.setOnClickListener(this);
+        purpleBtn.setOnClickListener(this);
+        redBtn.setOnClickListener(this);
+        yellowBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        // Find the button by ID and play the correct sound
+        int clickedBtnId = view.getId();
+
+        if (clickedBtnId == R.id.blackBtn){
+            PlaySounds(R.raw.black);
+        } else if (clickedBtnId == R.id.greenBtn){
+            PlaySounds(R.raw.green);
+        } else if (clickedBtnId == R.id.purpleBtn){
+            PlaySounds(R.raw.purple);
+        } else if (clickedBtnId == R.id.redBtn){
+            PlaySounds(R.raw.red);
+        } else if (clickedBtnId == R.id.yellowBtn){
+            PlaySounds(R.raw.yellow);
+        }
+    }
+
+    public void PlaySounds(int id){
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, id);
+        mediaPlayer.start();
+    }
+}
